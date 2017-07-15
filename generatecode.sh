c96b43a32bbc2b17e28a32fa71f7b04f4ce49ee7
Andrea Margheri <margheri.andrea@gmail.com>
2017-07-15 12:36:38 +0100

    ref to RI api in SUNFISH repository

--- generatecode.sh
+++ generatecode.sh
@@ -1,6 +1,6 @@
 
 #Creation of Server-stub
-swagger-codegen generate -i RegistryInterface.yaml -l nodejs-server -o ./server/
+swagger-codegen generate -i https://raw.githubusercontent.com/sunfish-prj/SUNFISH-Platform-API/newRI/RegistryInterfaceAPI/swagger/RegistryInterface.yaml -l nodejs-server -o ./server/
 
 #Documentation
-swagger-codegen generate -i https://petstore.swagger.io/v2/swagger.json -l dynamic-html -o .
+swagger-codegen generate -i https://raw.githubusercontent.com/sunfish-prj/SUNFISH-Platform-API/newRI/RegistryInterfaceAPI/swagger/RegistryInterface.yaml -l dynamic-html -o .
