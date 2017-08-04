2117246ad337540c007d022dd1fc443345668eb1
RunshanHu <runsh.hu@gmail.com>
2017-08-04 19:14:05 +0100

    autogen code according to new API

--- server/controllers/Anonymisation.js
+++ server/controllers/Anonymisation.js
@@ -4,10 +4,14 @@ var url = require('url');
 
 var Anonymisation = require('./AnonymisationService');
 
-module.exports.anonymisationRegisterPOST = function anonymisationRegisterPOST (req, res, next) {
-  Anonymisation.anonymisationRegisterPOST(req.swagger.params, res, next);
+module.exports.anonymisationQueryOldResPOST = function anonymisationQueryOldResPOST (req, res, next) {
+  Anonymisation.anonymisationQueryOldResPOST(req.swagger.params, res, next);
 };
 
-module.exports.anonymisationStatisticsPOST = function anonymisationStatisticsPOST (req, res, next) {
-  Anonymisation.anonymisationStatisticsPOST(req.swagger.params, res, next);
+module.exports.anonymisationReceiveAnonyResPOST = function anonymisationReceiveAnonyResPOST (req, res, next) {
+  Anonymisation.anonymisationReceiveAnonyResPOST(req.swagger.params, res, next);
+};
+
+module.exports.anonymisationRegisterPOST = function anonymisationRegisterPOST (req, res, next) {
+  Anonymisation.anonymisationRegisterPOST(req.swagger.params, res, next);
 };
