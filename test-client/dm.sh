cd488d8f759961b35d4f0362dfda5b123b0b845a
Andrea Margheri <margheri.andrea@gmail.com>
2017-08-04 12:50:13 +0200

    params for tests

--- test-client/dm.sh
+++ test-client/dm.sh
@@ -1,20 +1,28 @@
+#!/bin/bash
+
+echo
+
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
   "requestorID": "string",
   "token": "string",
   "dataID": "string",
   "key": "chiave2"
-}' 'http://$1/ri/dm/store'
+}' 'http://'$1':'$2'/ri/dm/store'
 
-
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
-  "requestorID": "string",
-  "token": "string",
-  "dataId": "string"
-}' 'http://$1/ri/dm/read'
-
+echo
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
   "requestorID": "string",
   "token": "string",
   "dataId": "string"
-}' 'http://$1/ri/dm/delete'
+}' 'http://'$1':'$2'/ri/dm/read'
+
+echo
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "requestorID": "string",
+  "token": "string",
+  "dataId": "string"
+}' 'http://'$1':'$2'/ri/dm/delete'
+
+echo 
