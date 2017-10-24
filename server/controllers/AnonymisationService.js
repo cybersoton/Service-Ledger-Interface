fb5d8001cd65982985237f64d15c2e1b4ab59908
RunshanHu <runsh.hu@gmail.com>
2017-10-24 15:10:01 +0000

    update utilitycheck

--- server/controllers/AnonymisationService.js
+++ server/controllers/AnonymisationService.js
@@ -16,15 +16,45 @@ exports.anonymisationRegisterToRegistry = function(args, res, next) {
    **/
   if(debug) console.log(`--->RI: anonymisationRegisterToRegistry method called`);
   var examples = {};
-  examples['application/json'] = {
-  "message" : "register successfully"
-};
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+      examples['application/json'] = {
+      "message" : "register failed"
+  };
+
+  rp({
+      method: 'POST',
+      // uri: 'http://localhost:XXXX/r/put',
+      uri: url.format({
+            protocol: 'http',
+            hostname: request_parameters.registry.ip,
+            port: request_parameters.registry.port,
+            pathname: request_parameters.path.registry_put
+      }),
+      body: {
+                "key": JSON.stringify(args.body.value),
+                "value": JSON.stringify(args.body.value)
+            },
+      header: {'User-Agent': 'Registry-Interface'},
+      json: true
+  }).then(response => {
+  
+      examples['application/json'].message = response.message;
+
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+
+  }).catch(err => {
+      console.log("---->RI: register to Registry failed!");
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  });
 }
 
 exports.anonymisationQueryOldRes = function(args, res, next) {
@@ -83,24 +113,48 @@ exports.anonymisationQueryOldRes = function(args, res, next) {
         if(args.body.value.request_budget < remain_budge){
             examples['application/json'].budget_used = args.body.value.request_budget;
         } else {
+            // budget used up
             examples['application/json'].budget_used = -1;
         }
     }
+    if (Object.keys(examples).length > 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+    } else {
+        res.end();
+    }
   }).catch(err => {
       if(err.statusCode == 404) {
-        console.log("----> Id not found!")
+          console.log("----> Id not found!")
+
+          let old_result_exist = false;
+          let small_budget = 0.5;
+          if(old_result_exist){
+              examples['application/json'].ifExist = 1;
+              examples['application/json'].budget_used = small_budget;
+          } else {
+              //if old result not exist
+              examples['application/json'].ifExist = 0;
+              let remain_budget = 10;
+              if(args.body.value.request_budget < remain_budge){
+                  examples['application/json'].budget_used = args.body.value.request_budget;
+              } else {
+                  // budget used up
+                  examples['application/json'].budget_used = -1;
+              }
+          }
+          if (Object.keys(examples).length > 0) {
+              res.setHeader('Content-Type', 'application/json');
+              res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+          } else {
+              res.end();
+          }
       } else {
-        console.error(`---->error when request!`);
-        // console.log(err);
+           console.error(`---->error when request!`);
+           // console.log(err);
       }
   });
 
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
 }
 
 exports.anonymisationReceiveAnonyRes = function(args, res, next) {
@@ -113,19 +167,45 @@ exports.anonymisationReceiveAnonyRes = function(args, res, next) {
    **/
   if(debug) console.log(`--->RI: anonymisationReceiveAnonyRes method called`);
 
-  //perform utility check here
-
   var examples = {};
   examples['application/json'] = {
-     "final_status": 1,
-     "final_result": 111
+     "final_status": -1,
+     "final_result": -111
   };
-  if (Object.keys(examples).length > 0) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-      res.end();
-  }
+
+  //perform utility check here
+  rp({
+      method: 'POST',
+      // uri: 'http://localhost:60005/r/get',
+      uri: url.format({
+            protocol: 'http',
+            hostname: request_parameters.registry.ip,
+            port: request_parameters.registry.port,
+            pathname: request_parameters.path.registry_get
+      }),
+      body: {"key": JSON.stringify(args.body.value)},
+      header: {'User-Agent': 'Registry-Interface'},
+      json: true
+  }).then(response => {
+      examples['application/json'].final_result = anonymised_result;
+      examples['application/json'].final_status = 1; 
+      
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  }).catch(err => {
+      console.log("---->error when utility checking")
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  });
+
 }
 
 exports.anonymisationUpdateLedger = function(args, res, next) {
@@ -157,25 +237,19 @@ exports.anonymisationUpdateLedger = function(args, res, next) {
       header: {'User-Agent': 'Registry-Interface'},
       json: true
   }).then(response => {
-     examples['application/json'].message = response.message; 
      if(debug) {
          console.log("---->response from Registry: ");
          console.log(response);
      }
+     examples['application/json'].message = response.message; 
+     if (Object.keys(examples).length > 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+     } else {
+        res.end();
+     }
   }).catch(err => console.log(err));
 
-  if(debug) console.log(Object.keys(examples).length);
-
-  if (Object.keys(examples).length > 0) {
-      res.setHeader('Content-Type', 'application/json');
-      if(debug) {
-          console.log("-------->before send: ");
-          console.log(examples[Object.keys(examples)[0]]);
-      }
-      res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-      res.end();
-  }
 }
 
 
