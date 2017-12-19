a46839afc522989b495a4a7e691b16098fe242a7
Andrea Margheri <margheri.andrea@gmail.com>
2017-12-19 22:53:24 +0100

    updated stub per proposal

--- server/controllers/Proposal.js
+++ server/controllers/Proposal.js
@@ -4,6 +4,10 @@ var url = require('url');
 
 var Proposal = require('./ProposalService');
 
+module.exports.proposalCountVotesPOST = function proposalCountVotesPOST (req, res, next) {
+  Proposal.proposalCountVotesPOST(req.swagger.params, res, next);
+};
+
 module.exports.proposalGetProposalPOST = function proposalGetProposalPOST (req, res, next) {
   Proposal.proposalGetProposalPOST(req.swagger.params, res, next);
 };
--- server/controllers/ProposalService.js
+++ server/controllers/ProposalService.js
@@ -1,5 +1,26 @@
 'use strict';
 
+exports.proposalCountVotesPOST = function(args, res, next) {
+  /**
+   * Request a votes counting to validate a stored proposal  
+   *
+   * body Countvotes-proposal-body Body in JSON
+   * returns countvotes-proposal-response
+   **/
+  var examples = {};
+  examples['application/json'] = {
+  "requestorID" : "aeiou",
+  "proposalStatus" : "aeiou",
+  "proposalID" : "aeiou"
+};
+  if (Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  } else {
+    res.end();
+  }
+}
+
 exports.proposalGetProposalPOST = function(args, res, next) {
   /**
    * getting a proposal 
@@ -9,14 +30,13 @@ exports.proposalGetProposalPOST = function(args, res, next) {
    **/
   var examples = {};
   examples['application/json'] = {
-  "proposal" : [ {
-    "proposerId" : "aeiou",
-    "proposalQuorum" : "aeiou",
-    "proposalDescription" : "aeiou"
-  } ],
+  "requestorID" : "aeiou",
   "proposalStatus" : "aeiou",
-  "typeProposal" : "aeiou",
-  "proposalID" : "aeiou"
+  "votersNumber" : "aeiou",
+  "proposalType" : "aeiou",
+  "proposalQuorum" : "aeiou",
+  "proposalID" : "aeiou",
+  "proposalDescription" : "aeiou"
 };
   if (Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
@@ -47,7 +67,7 @@ exports.proposalSubmitProposalPOST = function(args, res, next) {
 
 exports.proposalVoteProposalPOST = function(args, res, next) {
   /**
-   * Submitting a proposal  
+   * vote for a submitted a proposal  
    *
    * body Vote-proposal-body Body in JSON
    * returns ack-response
